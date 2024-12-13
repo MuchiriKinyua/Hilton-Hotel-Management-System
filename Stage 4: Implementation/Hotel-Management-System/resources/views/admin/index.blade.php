4365c246d9e0c421593c8d6758c021fab5bf59ff
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-13 15:02:16 +0300

    Integrating Admin Template

--- Stage 4: Implementation/Hotel-Management-System/resources/views/admin/index.blade.php
+++ Stage 4: Implementation/Hotel-Management-System/resources/views/admin/index.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-</x-app-layout>
-
-</body>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+  @include('admin.header')
+  @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+    @include('admin.body')
+        @include('admin.footer')
+  </body>
 </html>
