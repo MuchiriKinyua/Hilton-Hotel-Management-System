258712748a1fe28118b28efc6ad399f220fc30b2
MuchiriKinyua <muchirikinyua2000@gmail.com>
2025-01-23 07:14:02 +0300

    transaction columns

--- Stage 4: Implementation/Hotel-Management-System/app/Models/Transactions.php
+++ Stage 4: Implementation/Hotel-Management-System/app/Models/Transactions.php
@@ -10,6 +10,10 @@ class Transactions extends Model
     use HasFactory;
 
     protected $fillable = [
+        'transaction_reference',
+        'amount_paid',
+        'payment_date',
+        'payment_method',
         'daily_rate',
         'total_stay_cost',
         'number_of_nights',
